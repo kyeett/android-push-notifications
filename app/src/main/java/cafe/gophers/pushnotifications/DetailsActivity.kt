8fca81c6a249bc97b3edb6f0c8cbabbc1d37a329
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2019-10-06 23:50:43 +0200

    go to specific view

---
+++ app/src/main/java/cafe/gophers/pushnotifications/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package cafe.gophers.pushnotifications
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
--- app/src/main/java/cafe/gophers/pushnotifications/MainActivity.kt
+++ app/src/main/java/cafe/gophers/pushnotifications/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import android.app.NotificationChannel
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.text.TextUtils
@@ -47,11 +49,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun sendNotification() {
+        val intent = Intent(this, DetailsActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+
         // Note: if Android API level is less than 26, the channelID will be ignored
         val notificationBuilder = NotificationCompat.Builder(this, channelID)
             .setContentTitle("Hello Notification! \uD83D\uDC7B")
             .setContentText("This is the notification text \uD83D\uDC4F")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
+            /* Added */
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(1, notificationBuilder.build())
